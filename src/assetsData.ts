ec605b8169ce29f62c455275aff4054e0252ce00
ItsPi3141 <90981829+ItsPi3141@users.noreply.github.com>
2025-03-29 14:40:25 -0700

    feat: add custom asset matchers

--- src/assetsData.ts
+++ src/assetsData.ts
@@ -65,10 +65,61 @@ const _assets: Record<string, LanguageAsset> = {
 		default: "java",
 	},
 	js: {
-		custom: [],
+		custom: [
+			{
+				matcher: "esbuild.",
+				asset: "esbuild",
+			},
+			{
+				matcher: ".pnpmfile.",
+				asset: "pnpm",
+			},
+			{
+				matcher: "rollup.config.",
+				asset: "rollup",
+			},
+			{
+				matcher: "next.config.",
+				asset: "vercel",
+			},
+			{
+				matcher: "vite.config.",
+				asset: "vite",
+			},
+			{
+				matcher: "webpack.",
+				asset: "webpack",
+			},
+			{
+				matcher: "yarn.config.",
+				asset: "yarn",
+			},
+		],
 		default: "javascript",
 	},
 	json: {
+		custom: [
+			{
+				matcher: "deno.",
+				asset: "deno",
+			},
+			{
+				matcher: "fly.json",
+				asset: "fly",
+			},
+			{
+				matcher: "netlify.json",
+				asset: "netlify",
+			},
+			{
+				matcher: /package(-lock)?.json/i,
+				asset: "npm",
+			},
+			{
+				matcher: "vercel.json",
+				asset: "vercel",
+			},
+		],
 		default: "json",
 	},
 	jsx: {
@@ -79,6 +130,32 @@ const _assets: Record<string, LanguageAsset> = {
 	},
 	kt: {
 		default: "kotlin",
+	},
+	lock: {
+		custom: [
+			{
+				matcher: "bun.lock",
+				asset: "bun",
+			},
+			{
+				matcher: "deno.lock",
+				asset: "deno",
+			},
+			{
+				matcher: "yarn.lock",
+				asset: "yarn",
+			},
+		],
+		default: "lock",
+	},
+	log: {
+		custom: [
+			{
+				matcher: "yarn-error.log",
+				asset: "yarn",
+			},
+		],
+		default: "text",
 	},
 	ls: {
 		default: "livescript",
@@ -91,6 +168,12 @@ const _assets: Record<string, LanguageAsset> = {
 	},
 	nim: {
 		default: "nim",
+	},
+	npmignore: {
+		default: "npm",
+	},
+	npmrc: {
+		default: "npm",
 	},
 	php: {
 		default: "php",
@@ -123,13 +206,52 @@ const _assets: Record<string, LanguageAsset> = {
 		default: "swift",
 	},
 	toml: {
+		custom: [
+			{
+				matcher: "fly.toml",
+				asset: "fly",
+			},
+			{
+				matcher: "netlify.toml",
+				asset: "netlify",
+			},
+		],
 		default: "toml",
 	},
 	ts: {
+		custom: [
+			{
+				matcher: "esbuild.",
+				asset: "esbuild",
+			},
+			{
+				matcher: "rollup.config.",
+				asset: "rollup",
+			},
+			{
+				matcher: "next.config.",
+				asset: "vercel",
+			},
+			{
+				matcher: "vite.config.",
+				asset: "vite",
+			},
+			{
+				matcher: "webpack.",
+				asset: "webpack",
+			},
+			{
+				matcher: "yarn.config.",
+				asset: "yarn",
+			},
+		],
 		default: "typescript",
 	},
 	v: {
 		default: "v",
+	},
+	vercelignore: {
+		default: "vercel",
 	},
 	wat: {
 		default: "wasm",
@@ -138,7 +260,34 @@ const _assets: Record<string, LanguageAsset> = {
 		default: "xml",
 	},
 	yaml: {
+		custom: [
+			{
+				matcher: "fly.y",
+				asset: "fly",
+			},
+			{
+				matcher: "netlify.y",
+				asset: "netlify",
+			},
+			{
+				matcher: /pnpm-\w+\.y/,
+				asset: "pnpm",
+			},
+			{
+				matcher: ".yarnrc.y",
+				asset: "yarn",
+			},
+		],
 		default: "yaml",
+	},
+	yarnclean: {
+		default: "yarn",
+	},
+	yarnrc: {
+		default: "yarn",
+	},
+	"yarn-integrity": {
+		default: "yarn",
 	},
 	zig: {
 		default: "zig",
@@ -155,6 +304,7 @@ const aliases: Record<string, string[]> = {
 	js: ["cjs", "mjs"],
 	json: ["jsonc"],
 	jsx: ["tsx"],
+	lock: ["lockb"],
 	md: ["markdown", "mdx"],
 	ml: ["cmt", "cmti", "codoc", "mli", "odoc"],
 	nim: ["nims", "nimble"],
