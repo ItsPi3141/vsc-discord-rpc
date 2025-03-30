ac8bb2d303535ea56641e8e2b410cb6ebd1f2f5f
ItsPi3141 <90981829+ItsPi3141@users.noreply.github.com>
2025-03-29 21:22:58 -0700

    feat: prevent icon caching

--- src/assets.ts
+++ src/assets.ts
@@ -2,6 +2,8 @@ import { assets } from "./assetsData";
 
 const basePath =
 	"https://raw.githubusercontent.com/itspi3141/vsc-discord-rpc/refs/heads/main/assets/icons/";
+const timestamp = Date.now();
+
 export const getAsset = ({
 	fileName,
 	filePath,
@@ -11,7 +13,7 @@ export const getAsset = ({
 	filePath?: string;
 	name?: string;
 }): string | undefined => {
-	if (name) return `${basePath}${name}.png`;
+	if (name) return `${basePath}${name}.png?v=${timestamp}`;
 
 	if (!fileName || !filePath) return undefined;
 
@@ -19,19 +21,19 @@ export const getAsset = ({
 	if (!ext) return undefined;
 
 	const lang = assets[ext];
-	if (!lang) return `${basePath}text.png`;
+	if (!lang) return `${basePath}text.png?v=${timestamp}`;
 	if (lang.custom) {
 		for (const custom of lang.custom) {
 			if (custom.matcher instanceof RegExp && custom.matcher.test(filePath))
-				return `${basePath}${custom.asset}.png`;
+				return `${basePath}${custom.asset}.png?v=${timestamp}`;
 
 			if (typeof custom.matcher === "string") {
 				if (custom.matcher.includes("/") && filePath.includes(custom.matcher))
-					return `${basePath}${custom.asset}.png`;
+					return `${basePath}${custom.asset}.png?v=${timestamp}`;
 				if (fileName.includes(custom.matcher))
-					return `${basePath}${custom.asset}.png`;
+					return `${basePath}${custom.asset}.png?v=${timestamp}`;
 			}
 		}
 	}
-	return `${basePath}${lang.default}.png`;
+	return `${basePath}${lang.default}.png?v=${timestamp}`;
 };
